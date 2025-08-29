980bec50c2fa75b1b93c9bae701f2fbfb0b23f01
hackman300 <tcmichel22@gmail.com>
2025-08-28 22:40:30 -0400

    product_catalog.py

--- product_catalog.py
+++ product_catalog.py
@@ -1,29 +1,25 @@
 from product_data import products
-# TODO: Step 1 - Print out the products to see the data that you are working with.
-
-
+for i in products:
+    print(i)
 
 # TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
 
-
+customer_preferences = []
 response = ""
 while response != "N":
-    print("Input a preference:")
+    print("Input a preference: ")
     preference = input()
-    # Add the customer preference to the list
-
+    customer_preferences.append(preference)  # Add the customer preference to the list
     response = input("Do you want to add another preference? (Y/N): ").upper()
-  
 
 # TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
-
-
+customer_tags = set(customer_preferences)
 
 # TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
 converted_products = []
-
-
-
+for product in products:
+    converted_product = {'name': product['name'],'tags': set(product['tags'])}
+    converted_products.append(converted_product)
 
 # TODO: Step 5 - Write a function to calculate the number of matching tags
 def count_matches(product_tags, customer_tags):
@@ -34,10 +30,7 @@ def count_matches(product_tags, customer_tags):
     Returns:
         int: The number of matching tags between the product and customer.
     '''
-    pass
-
-
-
+    return len(product_tags.intersection(customer_tags))
 
 # TODO: Step 6 - Write a function that loops over all products and returns a sorted list of matches
 def recommend_products(products, customer_tags):
@@ -48,15 +41,35 @@ def recommend_products(products, customer_tags):
     Returns:
         list: A list of products containing product names and their match counts.
     '''
-    pass
-
-
+    matches = []
+    for product in products:
+        match_count = count_matches(product['tags'], customer_tags)
+        if match_count > 0:  # Only include products with at least one match
+            matches.append({
+                'name': product['name'],
+                'matches': match_count
+            })
+    # Sort by match count in descending order
+    return sorted(matches, key=lambda x: x['matches'], reverse=True)
 
 # TODO: Step 7 - Call your function and print the results
-
-
-
+recommendations = recommend_products(converted_products, customer_tags)
+print("Recommended Products:")
+for rec in recommendations:
+    print(f"Product: {rec['name']}, Matches: {rec['matches']}")
 
 # DESIGN MEMO (write below in a comment):
 # 1. What core operations did you use (e.g., intersections, loops)? Why?
-# 2. How might this code change if you had 1000+ products?
+    '''
+The core operations that were used in this script were sets, lists, sorting, intersections, while/for loops, and functions. Sets were 
+used to get rid of duplicates in customer preferences and to allow efficient tag comparisons. Sets also allow for the use of the "all or nothing" lookup 
+method, which tries to find an exact match to the input provided. The intersection method was used to compare product and customer tags to find matches. The for loops were used 
+to iterate through all the products for tag conversion and recommendation generation, which allowed searching through the entire list of products, while the while 
+loop was used to allow the user to continue adding preferences until the letter "N" was inputted. The sorting method was used to order recommendations by matching 
+the count in descending order from the most relevant products to the least relevant. List append methods were used to build the "customer_preferences" 
+list and the recommendation output list to collect data. These operations were used because of their efficiency and usability. Sets allow for faster matching compared 
+to lists. Sorting ensures that the best matches are presented first. For loop methods make sure all data is iterated to find the correct result. 
+
+Something that I would change if there were 1000 products would be to add a method so that I don't have to iterate through over 1000 products. Another would 
+be to add a database for better organization and management. Lastly, multi-threading/processing would be added for multitasking.     
+    '''
